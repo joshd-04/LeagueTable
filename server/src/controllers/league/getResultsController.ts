b79908b02466f03ff1278f8afe6bdd6e961abada
Joshua Dyal <joshdyal04@gmail.com>
2026-01-01 15:00:22 +0000

    modified getResultsController.ts for robustness and matchweek & season filter query

--- server/src/controllers/league/getResultsController.ts
+++ server/src/controllers/league/getResultsController.ts
@@ -4,8 +4,10 @@ import {
   IFixtureSchema,
   ILeagueSchema,
   IResultSchema,
+  IUserSchema,
 } from '../../util/definitions';
 import { ErrorHandling } from '../../util/errorChecking';
+import { shouldGrantAccessToFeature } from '../../util/helpers';
 
 export async function getResultsController(
   req: Request,
@@ -28,10 +30,13 @@ export async function getResultsController(
 
     // Check if league exists
     try {
-      league = await League.findById(leagueId).populate({
-        path: 'results',
-        populate: [{ path: 'homeTeamDetails' }, { path: 'awayTeamDetails' }],
-      });
+      league = await League.findById(leagueId).populate([
+        {
+          path: 'results',
+          populate: [{ path: 'homeTeamDetails' }, { path: 'awayTeamDetails' }],
+        },
+        { path: 'leagueOwner' },
+      ]);
     } catch {
       return next(
         new ErrorHandling(404, {
@@ -58,37 +63,142 @@ export async function getResultsController(
       );
     }
 
-    if (league.leagueLevel === 'free' || req.query.season === undefined) {
-      // Get this seasons results
-      allResults = allResults.filter(
-        (result) => result.season === league.currentSeason
-      );
-    } else {
+    /* 
+    IF the user has requested a specific season,
+
+        Find out what season they want
+        IF requested season is not a number,
+            RETURN error
+        ELSE IF requested season is not in valid range,
+            RETURN error
+        ELSE IF requested season is current season,
+            RETURN current season's data
+        ELSE (requested season is a number, is in the valid range and is not the current season)
+            DETERMINE if the season rewind is allowed
+            IF season rewind allowed,
+                RETURN requested season's data
+            ELSE
+                RETURN error
+    
+    ELSE
+        RETURN current season's data
+    */
+
+    const requestedSeason = req.query.season;
+    const accountType = (league.leagueOwner as unknown as IUserSchema)
+      .accountType;
+
+    const allowSeasonRewind = shouldGrantAccessToFeature(
+      'pro',
+      league.leagueLevel,
+      accountType
+    );
+
+    let seasonFilter = league.currentSeason;
+
+    if (!!requestedSeason) {
+      const isValidNum = !Number.isNaN(+requestedSeason);
+
+      const inValidRange =
+        isValidNum &&
+        Number(requestedSeason) >= 1 &&
+        Number(requestedSeason) <= league.currentSeason;
+
       if (
-        req.query.season !== undefined &&
-        Number.isInteger(Number(req.query.season))
+        isValidNum &&
+        inValidRange &&
+        +requestedSeason !== league.currentSeason
       ) {
-        allResults = allResults.filter(
-          (result) => result.season === Number(req.query.season)
+        if (allowSeasonRewind) {
+          seasonFilter = +requestedSeason;
+        } else {
+          return next(
+            new ErrorHandling(403, {
+              message: `League owner must upgrade to PRO to view data for season ${requestedSeason}.`,
+            })
+          );
+        }
+      } else if (!isValidNum) {
+        return next(
+          new ErrorHandling(400, {
+            message: `Invalid season query given`,
+          })
         );
-      } else {
-        allResults = allResults.filter(
-          (result) => result.season === league.currentSeason
+      } else if (!inValidRange) {
+        return next(
+          new ErrorHandling(400, {
+            message: `Season query outside valid range`,
+          })
         );
+      } else if (+requestedSeason === league.currentSeason) {
+        seasonFilter = league.currentSeason;
       }
+    } else {
+      seasonFilter = league.currentSeason;
     }
 
-    if (!isNaN(Number(req.query.matchweek))) {
-      allResults = allResults.filter(
-        (result) => result.matchweek === Number(req.query.matchweek)
+    let results: IResultSchema[] = [];
+    results = allResults.filter((result) => result.season === seasonFilter);
+
+    // Matchweek query handling
+
+    /* 
+    IF the user has requested a specific matchweek,
+
+        Find out what matchweek they want
+        IF requested matchweek is not a number,
+            RETURN error
+        ELSE IF requested matchweek is not in valid range,
+            RETURN error
+        
+        ELSE (requested matchweek is a number and is in the valid range)
+            RETURN requested matchweek data
+    
+    ELSE
+        RETURN all matchweek data
+    */
+
+    const requestedMatchweek = req.query.matchweek;
+    let matchweekFilter: number | null = null;
+
+    if (!!requestedMatchweek) {
+      const isValidNum = !Number.isNaN(+requestedMatchweek);
+
+      const inValidRange =
+        isValidNum &&
+        Number(requestedMatchweek) >= 1 &&
+        Number(requestedMatchweek) <= league.finalMatchweek;
+
+      if (isValidNum && inValidRange) {
+        matchweekFilter = +requestedMatchweek;
+      } else if (!isValidNum) {
+        return next(
+          new ErrorHandling(400, {
+            message: `Invalid matchweek query given`,
+          })
+        );
+      } else if (!inValidRange) {
+        return next(
+          new ErrorHandling(400, {
+            message: `Matchweek query outside valid range. 1-${league.finalMatchweek}.`,
+          })
+        );
+      }
+    } else {
+      matchweekFilter = null;
+    }
+
+    if (matchweekFilter !== null) {
+      results = results.filter(
+        (result) => result.matchweek === matchweekFilter
       );
     }
 
     if (req.query.limit !== undefined && +req.query.limit > 0) {
-      allResults = allResults.slice(0, +req.query.limit);
+      results = results.slice(0, +req.query.limit);
     }
 
-    res.status(200).json({ status: 'success', data: { results: allResults } });
+    res.status(200).json({ status: 'success', data: { results: results } });
   } catch (e: any) {
     console.error(e);
     return next(
